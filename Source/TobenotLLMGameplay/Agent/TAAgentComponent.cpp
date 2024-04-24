100f56d79dba0a958c2f48038fa22114c40b83dc
tobenot <2818373670@qq.com>
2024-04-25 00:45:42 +0800

    禁止自言自语

--- Source/TobenotLLMGameplay/Agent/TAAgentComponent.cpp
+++ Source/TobenotLLMGameplay/Agent/TAAgentComponent.cpp
@@ -1,6 +1,7 @@
 #include "TAAgentComponent.h"
 #include "TimerManager.h"
 #include "Chat/Shout/TAShoutComponent.h"
+#include "Shout/TAShoutManager.h"
 
 UTAAgentComponent::UTAAgentComponent()
 {
@@ -40,11 +41,28 @@ void UTAAgentComponent::TickComponent(float DeltaTime, ELevelTick TickType, FAct
 
 void UTAAgentComponent::RequestSpeak()
 {
-	UTAShoutComponent* ShoutComponent = GetOwner()->FindComponentByClass<UTAShoutComponent>();
+	// 获取对 UTAShoutManager 的引用
+	UTAShoutManager* ShoutManager = GetWorld()->GetSubsystem<UTAShoutManager>();
+	if (!ShoutManager) return;
 
-	if(ShoutComponent)
+	// 获取周围范围内的 UTAShoutComponent 列表
+	float ShoutRange = 700.f;
+	TArray<UTAShoutComponent*> NearbyShoutComponents = ShoutManager->GetShoutComponentsInRange(GetOwner(), ShoutRange);
+
+	// 检查除了自己之外是否有其他 UTAShoutComponent
+	if (NearbyShoutComponents.Num() > 1 || (NearbyShoutComponents.Num() == 1 && NearbyShoutComponents[0] != GetOwner()->FindComponentByClass<UTAShoutComponent>()))
 	{
-		ShoutComponent->RequestToSpeak();
+		// 如果有其他组件，则请求发言
+		UTAShoutComponent* ShoutComponent = GetOwner()->FindComponentByClass<UTAShoutComponent>();
+		if (ShoutComponent)
+		{
+			ShoutComponent->RequestToSpeak();
+		}
+	}
+	else
+	{
+		// 如果没有其他接收者，则不发言
+		// 例如，你可以在这里记录一条日志或执行一些其他逻辑
 	}
 }
 
